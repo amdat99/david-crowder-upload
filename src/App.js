df61a944ff844bb11317bc32970e5ce509010cd2
amdat99 <aamirdat99@gmail.com>
2021-05-26 00:25:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.scss";
+import React, { useEffect, useState } from "react";
+
+import Logo from "./logo.jpg";
 
 function App() {
+  const [serverConnected, setServerConnected] = useState(false);
+
+  useEffect(() => {
+    verifyServer();
+  }, []);
+
+  useEffect(() => {
+    if (serverConnected === false) {
+      const interval = setInterval(() => onVerify(), 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [serverConnected]);
+
+  const onVerify = () => {
+    setInterval(function () {
+      verifyServer();
+    }, 2000);
+  };
+
+  const verifyServer = () => {
+    fetch("https://david-crowder-server.herokuapp.com/verify").then((data) => {
+      console.log(data);
+      if (data) {
+        setServerConnected(true);
+      }
+    });
+  };
+
+  console.log(serverConnected);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className=" App center">
+      <a
+        className="link"
+        href="https://davidcrowderweb.wixsite.com/website/contact-1"
+      >
+        ⬅ Back to the Website
+      </a>
+      <div className="email">
+        <img src={Logo} alt="Logo" />
+
+        <form
+          action="https://david-crowder-server.herokuapp.com/sendemail"
+          method="POST"
+          encType="multipart/form-data"
         >
-          Learn React
-        </a>
-      </header>
+          <p>
+            Please upload a image or video as well as a message detailing what
+            work you require{" "}
+          </p>
+          <span style={{ position: "relative", bottom: "10px" }}>
+            Check your email inbox for confirmation after clicking send{" "}
+          </span>
+          <div className="form-group">
+            <input
+              style={{ marginBottom: "10px" }}
+              class="form-control"
+              type="text"
+              name="name"
+              required
+              placeholder="name:"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              style={{ marginBottom: "10px" }}
+              type="email"
+              class="form-control"
+              name="email"
+              placeholder="email:"
+              required
+              id=""
+            />
+          </div>
+          <div className="form-group">
+            <input
+              style={{ marginBottom: "10px" }}
+              type="text"
+              class="form-control"
+              name="number"
+              placeholder="number:"
+              required
+              id=""
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              style={{ width: "80%", marginBottom: "10px" }}
+              required
+              class="form-control"
+              name="body"
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Message:"
+            ></textarea>
+          </div>
+          <label htmlFor="attachment">Attachment:</label>
+          <div className="form-group">
+            <input
+              style={{ marginBottom: "10px" }}
+              type="file"
+              required
+              class="form-control"
+              name="image"
+              id=""
+            />
+          </div>
+          <div className="form-group">
+            {serverConnected ? (
+              <button
+                style={{ marginBottom: "10px" }}
+                type="submit"
+                class="btn btn-block btn-danger"
+              >
+                Send
+              </button>
+            ) : (
+              <div
+                style={{ width: "10px", height: "10px" }}
+                className="loader"
+              ></div>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
